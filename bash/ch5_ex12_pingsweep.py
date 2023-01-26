a215c67372f4ceb598c6b117b9852cbe9703a423
farrir <d.sven.schubert@gmail.com>
2023-01-26 11:25:31 -0500

    pingsweep v2 final

--- bash/ch5_ex12_pingsweep.py
+++ bash/ch5_ex12_pingsweep.py
@@ -3,10 +3,10 @@ import os
 
 IPrange = sys.argv[1]
 startIP = int(sys.argv[2])
-endIP = int(sys.argv[3])
+endIP = int(sys.argv[3])+1
 
 for ip in range(startIP, endIP):
     target = "{}.{}".format(IPrange, ip)
-    response = os.system("ping -c 1 {} > /dev/null".format(target))
+    response = os.system("ping -c 1 {} > /dev/null 2>&1".format(target))
     if response == 0:
         print(target)
